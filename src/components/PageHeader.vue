bfe7c6296732d7f7d41e59bb395f9409b31c86da
Ojonugwa Alikali <alikali.ojonugwajustice@gmail.com>
2023-09-05 09:52:20 +0100

    Implement mobile-menu using antd library

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,40 +1,125 @@
 <script setup>
-import { useRoute } from 'vue-router'
+// import { useRoute } from 'vue-router'
+import ChocolateMenuIcon from '@/components/icons/IconChocolateMenu.vue'
 
-const route = useRoute()
+import { reactive, ref, h, computed, watch } from 'vue'
+import { AppstoreOutlined } from '@ant-design/icons-vue'
+// const route = useRoute()
+
+const isClicked = ref(false)
+
+const scaleDown = () => {
+  isClicked.value = true
+  setTimeout(() => {
+    isClicked.value = false
+  }, 200)
+}
+
+const buttonClass = computed(() => ({
+  'register-btn': true,
+  clicked: isClicked.value
+}))
+
+// Antd menu
+const selectedKeys = ref(['1'])
+const openKeys = ref([''])
+
+const isMenuOpen = ref(false)
+
+const titleClick = (e) => {
+  console.log('titleClick', e)
+}
+
+const handleClick = (e) => {
+  console.log('click', e)
+  openKeys.value = ['']
+}
+
+const toggleMenu = () => {
+  // openKeys.value = ['sub2']
+  isMenuOpen.value = !isMenuOpen.value
+}
+
+watch(isMenuOpen, (val) => {
+  if (val) {
+    openKeys.value = ['sub2']
+  } else {
+    openKeys.value = ['']
+  }
+})
 </script>
 
 <template>
-  <header class="">
-    <div class="bg-blue-theme px-5 py-7">
+  <header class="bg-blue-theme">
+    <div class="header-bg px-5 py-7">
       <div class="mx-auto flex max-w-screen-xl items-center justify-between gap-x-[5%]">
-        <RouterLink to="/"
-          ><img alt="Logo" src="@/assets/logo.svg" class="w-9/12 lg:w-full"
-        /></RouterLink>
+        <div class="flex items-center gap-x-3">
+          <button class="md:hidden" @click="toggleMenu"><ChocolateMenuIcon /></button>
+          <RouterLink to="/"
+            ><img alt="Logo" src="@/assets/logo.svg" class="w-9/12 lg:w-full"
+          /></RouterLink>
+        </div>
 
-        <nav class="hidden w-full justify-between lg:flex">
+        <nav class="hidden w-full max-w-screen-sm justify-between md:flex">
           <RouterLink to="/" class="nav-link">Home</RouterLink>
-          <RouterLink to="/" class="nav-link">Home</RouterLink>
-          <RouterLink to="/" class="nav-link">Home</RouterLink>
-          <RouterLink to="/" class="nav-link">Home</RouterLink>
-          <!-- <RouterLink
-            to="/about-me"
-            :class="`text-base font-medium ${
-              route.path === '/about-me' && 'border-b-orange border-b-2'
-            }`"
-            >About Me</RouterLink
-          >
-          <RouterLink
-            to="#"
-            :class="`text-base font-medium ${
-              route.path === '/contact-me' && 'border-b-orange border-b-2'
-            }`"
-            >Contact Me</RouterLink
-          > -->
+          <RouterLink to="/" class="nav-link">About us</RouterLink>
+          <RouterLink to="/" class="nav-link">Schedule</RouterLink>
+          <RouterLink to="/" class="nav-link">Speakers</RouterLink>
+          <RouterLink to="/" class="nav-link">Sponsors</RouterLink>
+          <RouterLink to="/" class="nav-link">FAQ</RouterLink>
         </nav>
 
-        <RouterLink to="/" class="register-btn"> Register now </RouterLink>
+        <RouterLink to="/" :class="buttonClass" @click="scaleDown">Register now</RouterLink>
       </div>
     </div>
   </header>
+
+  <a-menu
+    id="dddddd"
+    v-model:openKeys="openKeys"
+    v-model:selectedKeys="selectedKeys"
+    mode="inline"
+    @click="handleClick"
+    class="antd-custom-menu"
+  >
+    <a-sub-menu key="sub2" @titleClick="titleClick">
+      <template #icon>
+        <AppstoreOutlined />
+      </template>
+      <template #title>Navigation Two</template>
+      <a-menu-item key="Home">Home</a-menu-item>
+      <a-menu-item key="About us">About us</a-menu-item>
+      <a-menu-item key="Schedule">Schedule</a-menu-item>
+      <a-menu-item key="Speakers">Speakers</a-menu-item>
+      <a-menu-item key="Sponsors">Sponsors</a-menu-item>
+      <a-menu-item key="FAQ">FAQ</a-menu-item>
+    </a-sub-menu>
+  </a-menu>
 </template>
+
+<style scoped>
+.header-bg {
+  /* border: 3px solid linear-gradient(136deg, rgba(255, 255, 255, 0.2), rgb(255, 255, 255)); */
+  /*
+  backdrop-filter: blur(3.3px);
+  */
+  background: linear-gradient(
+      175deg,
+      rgba(177, 177, 177, 0.32) 5.24%,
+      rgba(54, 53, 103, 0.2) 51.01%
+    ),
+    rgba(0, 0, 128, 0.3);
+}
+.nav-link {
+  @apply font-Urbanist text-base font-medium text-white;
+}
+
+.register-btn {
+  @apply flex h-[40px] w-full max-w-[165px] items-center justify-center rounded-[80px] px-9 text-sm font-extrabold md:h-14 md:max-w-[196px]  md:text-lg;
+  background: linear-gradient(180deg, #fc711b 25.62%, #fc4608 100.89%);
+}
+
+.clicked {
+  transform: scale(0.9);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,61 +8,65 @@ import SmallYellowGlitterIcon from '@/components/icons/IconSmYellowGlitter.vue'
 </script>
 
 <template>
-  <div class="hidden bg-blue-theme pb-[386px] pt-14 lg:block">
-    <div class="mx-auto lg:max-w-5xl">
-      <div class="hero-grid">
-        <span class="hero-text">Discover</span>
-        <div class="mx-auto"><PurpleGlitterIcon /></div>
-        <div><TeamworkIcon /></div>
-      </div>
+  <div>
+    <div class="hidden bg-blue-theme pb-[386px] pt-14 lg:block">
+      <div class="mx-auto lg:max-w-5xl">
+        <div class="hero-grid">
+          <span class="hero-text">Discover</span>
+          <div class="mx-auto"><PurpleGlitterIcon /></div>
+          <div><TeamworkIcon /></div>
+        </div>
 
-      <div class="hero-grid">
-        <div><ConferenceIcon /></div>
-        <div class="mx-auto"><YellowGlitterIcon /></div>
-        <span class="hero-text">Network</span>
-      </div>
+        <div class="hero-grid">
+          <div><ConferenceIcon /></div>
+          <div class="mx-auto"><YellowGlitterIcon /></div>
+          <span class="hero-text">Network</span>
+        </div>
 
-      <div class="hero-grid">
-        <span class="hero-text">Innovate</span>
-        <div class="mx-auto"><PurpleGlitterIcon /></div>
+        <div class="hero-grid">
+          <span class="hero-text">Innovate</span>
+          <div class="mx-auto"><PurpleGlitterIcon /></div>
 
-        <div class="flex gap-x-10">
-          <div class="max-w-[163px]">
-            <span class="font-Urbanist text-base font-black uppercase text-[#F68F21]">Date:</span>
-            <p class="font-Urbanist text-[22px] text-white">Thurs Nov 16, Fri Nov 17, 2023</p>
-          </div>
-          <div class="max-w-[163px]">
-            <span class="font-Urbanist text-base font-black uppercase text-[#F68F21]">Date:</span>
-            <p class="font-Urbanist text-[22px] text-white">Thurs Nov 16, Fri Nov 17, 2023</p>
+          <div class="flex gap-x-10">
+            <div class="max-w-[163px]">
+              <span class="font-Urbanist text-base font-black uppercase text-[#F68F21]">Date:</span>
+              <p class="font-Urbanist text-[22px] text-white">Thurs Nov 16, Fri Nov 17, 2023</p>
+            </div>
+            <div class="max-w-[163px]">
+              <span class="font-Urbanist text-base font-black uppercase text-[#F68F21]"
+                >Venue:</span
+              >
+              <p class="font-Urbanist text-[22px] text-white">Calvin & Tina Tyler Ballroom</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 
-  <div class="block h-screen bg-blue-theme lg:hidden">
-    <div class="px-6 lg:px-0">
-      <div class="hero-grid">
-        <span class="hero-text">Discover</span>
+    <div class="block h-screen bg-blue-theme pt-8 lg:hidden">
+      <div class="px-6 lg:px-0">
+        <div class="hero-grid">
+          <span class="hero-text">Discover</span>
 
-        <div class="mx-auto"><SmallPurpleGlitterIcon /></div>
+          <div class="mx-auto"><SmallPurpleGlitterIcon /></div>
 
-        <span class="hero-text">Network</span>
+          <span class="hero-text">Network</span>
 
-        <div class="mx-auto"><SmallYellowGlitterIcon /></div>
+          <div class="mx-auto"><SmallYellowGlitterIcon /></div>
 
-        <span class="hero-text">Innovate</span>
+          <span class="hero-text">Innovate</span>
 
-        <div class="mx-auto"><SmallPurpleGlitterIcon /></div>
+          <div class="mx-auto"><SmallPurpleGlitterIcon /></div>
 
-        <div class="flex gap-x-10">
-          <div class="max-w-[163px]">
-            <span class="font-Urbanist text-base font-black uppercase text-[#F68F21]">Date:</span>
-            <p class="font-Urbanist text-[22px] text-white">Thurs Nov 16, Fri Nov 17, 2023</p>
-          </div>
-          <div class="max-w-[163px]">
-            <span class="font-Urbanist text-base font-black uppercase text-[#F68F21]">Date:</span>
-            <p class="font-Urbanist text-[22px] text-white">Thurs Nov 16, Fri Nov 17, 2023</p>
+          <div class="flex gap-x-10">
+            <div class="max-w-[163px]">
+              <span class="font-Urbanist text-base font-black uppercase text-[#F68F21]">Date:</span>
+              <p class="font-Urbanist text-[22px] text-white">Thurs Nov 16, Fri Nov 17, 2023</p>
+            </div>
+            <div class="max-w-[163px]">
+              <span class="font-Urbanist text-base font-black uppercase text-[#F68F21]">Date:</span>
+              <p class="font-Urbanist text-[22px] text-white">Thurs Nov 16, Fri Nov 17, 2023</p>
+            </div>
           </div>
         </div>
       </div>
@@ -81,3 +85,21 @@ import SmallYellowGlitterIcon from '@/components/icons/IconSmYellowGlitter.vue'
     </div>
   </div>
 </template>
+
+<style scoped>
+.hero-grid {
+  @apply relative grid items-center justify-center gap-y-4 lg:grid-cols-[384px_0.4fr_384px] lg:gap-y-0;
+}
+
+.hero-text {
+  @apply block max-w-[376px] text-center font-Bebas text-8xl font-normal text-white lg:text-left lg:text-[112px];
+}
+
+.video-container {
+  @apply relative -mt-[100px] h-0 w-full pb-[56.25%] lg:-mt-[325px];
+}
+
+.video-container iframe {
+  @apply absolute left-0 top-0 h-full w-full rounded-[30px];
+}
+</style>
